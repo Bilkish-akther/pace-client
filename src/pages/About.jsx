8bb2d4011de466ccdefbe4c4a4188a7997c3ee5b
AbuZobayed <zobayed.ict05@gmail.com>
2024-06-04 19:22:10 +0600

    about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,40 +1,28 @@
 const About = () => {
   return (
     
-    <div>
-      this about
+   
+    <div className="container mx-auto  m-12">
+      <div className="grid grid-cols-2">
+        <div className="">
+          <img className="w-[600px]" src="/src/assets/img/about.png" alt="" />
+        </div>
+        <div className="">
+          <h1 className="text-7xl font-semibold text-green-600">About Us</h1>
+          <p className="mt-4">
+          A fruit is the part of a flowering plant that contains the seeds. The skin of a fruit may be thin, tough, or hard. Its insides are often sweet and juicy. But some fruits, including nuts, are dry. Fruits develop from a plants flowers. Some foods that people call vegetables are actually fruits.or example, cucumbers, squashes, and tomatoes are all fruits. They contain seeds, while true vegetables do not. True vegetables are the stems, leaves, or roots of plants.
+          </p>
+          <div className="card w-auto bg-base-100 mt-20 shadow-xl">
+            <div className="card-body">
+              <p>
+              There are two main types of fruit: fleshy fruits and dry fruits. In fleshy fruits, the part of the fruit around the seeds is juicy and soft. Apples, berries, oranges, and other fleshy fruits contain many seeds.{" "}
+              </p>
+            </div>
+          </div>
+          <button className="btn bg-green-600 mt-8 uppercase  text-white">Read more</button>
+        </div>
+      </div>
     </div>
-    // <div className="container mx-auto  m-12">
-    //   <div className="grid grid-cols-2">
-    //     <div className="">
-    //       <img className="w-[600px]" src="/src/assets/img/about.png" alt="" />
-    //     </div>
-    //     <div className="">
-    //       <h1 className="text-7xl font-semibold text-violet-600">About Us</h1>
-    //       <p className="mt-4">
-    //         A shoe is an item of footwear intended to protect and comfort the
-    //         human foot. Though the human foot can adapt to varied terrains and
-    //         climate conditions, it is vulnerable, and shoes provide protection.
-    //         Form was originally tied to function, but over time, shoes also
-    //         became fashion items. Some shoes are worn as safety equipment, such
-    //         as steel-toe boots, which are required footwear at industrial
-    //         worksites. Additionally, shoes have often evolved into many
-    //         different designs, such as high heels, which are most commonly worn
-    //         by women during fancy occasions. Contemporary footwear varies vastly
-    //         in style, complexity and cost. Basic sandals may consist of only a
-    //         thin sole and simple strap and be sold for a low cost.
-    //       </p>
-    //       <div className="card w-auto bg-base-100 mt-20 shadow-xl">
-    //         <div className="card-body">
-    //           <p>
-    //           The earliest known shoes are sagebrush bark sandals dating from approximately 7000 or 8000 BC, found in the Fort Rock Cave in the US state of Oregon in 1938.- <span className="font-bold">Earliest evidence</span>{" "}
-    //           </p>
-    //         </div>
-    //       </div>
-    //       <button className="btn bg-violet-600 mt-8 uppercase  text-white">Read more</button>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
